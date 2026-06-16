c8b412f39275948687b11f8cfc10ff622a4077c9
Aayush Singhal <aayushsinghal1984@gmail.com>
2026-06-17 00:05:01 +0530

    add code

--- terraform-bootstrap/provider.tf
+++ terraform-bootstrap/provider.tf
@@ -1,13 +1,21 @@
 terraform {
+    required_version = ">= 1.5.0"
+  cloud {
+    organization = "UL-ULSE"
+    workspaces {
+      name = "bootstrap-workspace"
+    }
+  }
+
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
-      version = "~> 0.50"
+      version = "4.14.0"
     }
   }
 }
 
 provider "tfe" {
-    hostname = "app.terraform.io"
-    token    = var.tfe_token #
+  hostname = "app.terraform.io"
+    token    = var.tfe_token
 }
