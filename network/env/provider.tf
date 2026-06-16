0869537331728850b09c3a3fe32efe6f44cd08a2
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-06-16 17:10:57 +0530

    Update provider.tf

--- network/env/provider.tf
+++ network/env/provider.tf
@@ -2,8 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
   cloud {
     organization = "ISCINFRA"
-
+    workspaces {
+      prefix = "infra-" 
+    }
   }
+}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
